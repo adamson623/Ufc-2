4b4c1542ce63793796c8a78557c60445d5d806d0
adamson623 <n448wgdvzs@privaterelay.appleid.com>
2026-07-13 01:58:31 +0700

    Update fighters.js

--- fighters.js
+++ fighters.js
@@ -1,1 +1,31 @@
+const leagues = {
 
+UAE_WARRIORS: {
+    LIGHTWEIGHT: {
+        champion: {},
+        rankings: []
+    }
+},
+
+ACA: {
+    LIGHTWEIGHT: {
+        champion: {},
+        rankings: []
+    }
+},
+
+PFL: {
+    LIGHTWEIGHT: {
+        champion: {},
+        rankings: []
+    }
+},
+
+UFC: {
+    LIGHTWEIGHT: {
+        champion: {},
+        rankings: []
+    }
+}
+
+};
