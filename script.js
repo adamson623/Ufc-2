a3b738d20ca95f0990c4d75131370d7912aa7d84
adamson623 <n448wgdvzs@privaterelay.appleid.com>
2026-07-13 01:43:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+function createFighter() {
 
+    let name = document.getElementById("name").value;
+    let country = document.getElementById("country").value;
+    let weight = document.getElementById("weight").value;
+    let style = document.getElementById("style").value;
+
+
+    if(name === "") {
+        alert("Введите имя бойца");
+        return;
+    }
+
+
+    document.getElementById("profile").innerHTML = `
+
+    🥊 <b>${name}</b><br><br>
+
+    🌍 Страна: ${country}<br>
+
+    ⚖️ Вес: ${weight}<br>
+
+    🥋 Стиль: ${style}<br><br>
+
+    📊 Рекорд: 0-0<br>
+
+    🏆 Организация: UAE Warriors
+
+    `;
+
+}
