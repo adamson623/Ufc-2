267da36d6976ca2b7c10a75bf1e5445070bcbf6a
adamson623 <n448wgdvzs@privaterelay.appleid.com>
2026-07-13 02:02:33 +0700

    Update fighters.js

--- fighters.js
+++ fighters.js
@@ -2,8 +2,128 @@ const leagues = {
 
 UAE_WARRIORS: {
     LIGHTWEIGHT: {
-        champion: {},
-        rankings: []
+        champion: {}champion: {
+    name: "Yusuf Al-Farsi",
+    country: "🇦🇪 ОАЭ",
+    record: "19-2",
+    style: "Универсал",
+    level: 86
+},,
+        rankings: [rankings: [
+
+{
+rank: 1,
+name: "Abdul Karim",
+country: "🇦🇪 ОАЭ",
+record: "16-2",
+style: "Универсал",
+level: 82
+},
+
+{
+rank: 2,
+name: "Rashid Aliev",
+country: "🇷🇺 Россия",
+record: "14-3",
+style: "Борьба",
+level: 80
+},
+
+{
+rank: 3,
+name: "Murad Khan",
+country: "🇦🇿 Азербайджан",
+record: "13-2",
+style: "Бокс",
+level: 79
+},
+
+{
+rank: 4,
+name: "Omar Silva",
+country: "🇧🇷 Бразилия",
+record: "12-3",
+style: "BJJ",
+level: 78
+},
+
+{
+rank: 5,
+name: "Alex Carter",
+country: "🇺🇸 США",
+record: "15-4",
+style: "Кикбоксинг",
+level: 77
+},
+
+{
+rank: 6,
+name: "Yuki Tanaka",
+country: "🇯🇵 Япония",
+record: "11-2",
+style: "Универсал",
+level: 76
+},
+
+{
+rank: 7,
+name: "Bek Nur",
+country: "🇰🇿 Казахстан",
+record: "10-2",
+style: "Борьба",
+level: 75
+},
+
+{
+rank: 8,
+name: "Leo Martins",
+country: "🇵🇹 Португалия",
+record: "11-4",
+style: "Бокс",
+level: 74
+},
+
+{
+rank: 9,
+name: "Adam Walker",
+country: "🇬🇧 Англия",
+record: "10-3",
+style: "Кикбоксинг",
+level: 73
+},
+
+{
+rank: 10,
+name: "Sami Hassan",
+country: "🇪🇬 Египет",
+record: "9-2",
+style: "Борьба",
+level: 72
+}
+{
+rank: 11,
+name: "Niko Petrov",
+country: "🇧🇬 Болгария",
+record: "9-3",
+style: "Борьба",
+level: 71
+},
+
+{
+rank: 12,
+name: "Hassan Karim",
+country: "🇹🇳 Тунис",
+record: "8-2",
+style: "Бокс",
+level: 70
+},
+
+{
+rank: 13,
+name: "Diego Costa",
+country: "🇪🇸 Испания",
+record: "10-4",
+]]
     }
 },
 
